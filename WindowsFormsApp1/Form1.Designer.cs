4ab78ad4bc2752a1eb35a7bf48cc91ca752ab7f0
Юрий Кялов <kyalov_uv@ProIT2014.onmicrosoft.com>
2022-09-08 22:17:34 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Done = new System.Windows.Forms.Button();
+            this.sumCredit = new System.Windows.Forms.TextBox();
+            this.percentCredit = new System.Windows.Forms.TextBox();
+            this.countMount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Done
+            // 
+            this.Done.Location = new System.Drawing.Point(15, 105);
+            this.Done.Name = "Done";
+            this.Done.Size = new System.Drawing.Size(216, 23);
+            this.Done.TabIndex = 0;
+            this.Done.Text = "Рассчитать график платежей";
+            this.Done.UseVisualStyleBackColor = true;
+            this.Done.Click += new System.EventHandler(this.Done_Click);
+            // 
+            // sumCredit
+            // 
+            this.sumCredit.Location = new System.Drawing.Point(131, 27);
+            this.sumCredit.Name = "sumCredit";
+            this.sumCredit.Size = new System.Drawing.Size(100, 20);
+            this.sumCredit.TabIndex = 1;
+            // 
+            // percentCredit
+            // 
+            this.percentCredit.Location = new System.Drawing.Point(131, 53);
+            this.percentCredit.Name = "percentCredit";
+            this.percentCredit.Size = new System.Drawing.Size(100, 20);
+            this.percentCredit.TabIndex = 2;
+            // 
+            // countMount
+            // 
+            this.countMount.Location = new System.Drawing.Point(131, 79);
+            this.countMount.Name = "countMount";
+            this.countMount.Size = new System.Drawing.Size(100, 20);
+            this.countMount.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Сумма кредита";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ставка %";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Количество месяцев";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(237, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(621, 116);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 137);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.countMount);
+            this.Controls.Add(this.percentCredit);
+            this.Controls.Add(this.sumCredit);
+            this.Controls.Add(this.Done);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Done;
+        private System.Windows.Forms.TextBox sumCredit;
+        private System.Windows.Forms.TextBox percentCredit;
+        private System.Windows.Forms.TextBox countMount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Done_Click(object sender, EventArgs e)
+        {
+            double sC = Convert.ToDouble(this.sumCredit.Text);
+            double percent = Convert.ToDouble(this.percentCredit.Text);
+            int cMount = Convert.ToInt32(this.countMount.Text);
+            double percStavka = percent / (100 * 12);
+            double pe = sC * (percStavka + percStavka / (Math.Pow((1 + percStavka), cMount) - 1));
+            int i = 0;
+            string str = "Месяц | Ежемесячный платеж | Основной долг | Долг по процентам | Остаток основного долга\n";
+            DataSet ds = new DataSet();
+            ds.Tables.Add(new DataTable());
+            ds.Tables[0].Columns.Add("Месяц");
+            ds.Tables[0].Columns.Add("Ежемесячный платеж");
+            ds.Tables[0].Columns.Add("Основной долг");
+            ds.Tables[0].Columns.Add("Долг по процентам");
+            ds.Tables[0].Columns.Add("Остаток основного долга");
+            while (sC > 0)
+            {
+                double Ipe = sC * percStavka;
+                double mainPe = pe - Ipe;
+                sC -= mainPe;
+                i += 1;
+                str+=$"{i}|{pe}|{mainPe}|{Ipe}|{sC}";
+                ds.Tables[0].Rows.Add(i,Math.Round(pe,2),Math.Round(mainPe,2),Math.Round(Ipe,2),Math.Round(sC,2));
+            }
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
